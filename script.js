2b01a8b70266ceaf7f0c11ebba57377d890dbb0f
Seksenyirmi <seksenyirmi@Seksenyirmis-MacBook-Air.local>
2023-08-21 11:48:41 +0300

    calculate process completed

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
+let firstValue = 0;
+let option = ""
+
 function clearButton() {
     document.getElementById("result").value = " ";
 }
 function addNumber(value) {
-    document.getElementById("result").value +=value;
+    document.getElementById("result").value += value;
+}
+
+function plus(){
+
+    firstValue = document.getElementById("result").value;
+    document.getElementById("result").value = "";
+
+    option = "plus"
+
+}
+function minus(){
+    firstValue = document.getElementById("result").value;
+    document.getElementById("result").value = "";
+
+    option = "minus"
+}
+function multiple(){
+    firstValue = document.getElementById("result").value;
+    document.getElementById("result").value= "";
+
+    option="multiple"
+}
+function divide(){
+    firstValue = document.getElementById("result").value;
+    document.getElementById("result").value= "";
+
+    option="divide"
+}
+function percent(){
+    firstValue = document.getElementById("result").value;
+    document.getElementById("result").value= "";
+
+    option="percent"
+}
+
+function equal(){
+    const secondValue = document.getElementById("result").value;
+    
+    if (option == "plus"){
+        document.getElementById("result").value = parseInt(firstValue) + parseInt(secondValue);
+    }
+    else if (option == "minus"){
+        document.getElementById("result").value = parseInt(firstValue) - parseInt(secondValue);
+    }
+    else if(option == "multiple"){
+        document.getElementById("result").value = parseInt(firstValue) * parseInt(secondValue);
+    }
+    else if(option == "divide"){
+        document.getElementById("result").value = parseInt(firstValue) / parseInt(secondValue);
+    }
+    else if(option == "percent"){
+        document.getElementById("result").value = parseInt(firstValue) % parseInt(secondValue);
+    }
 }
